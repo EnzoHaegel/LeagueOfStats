7f41d24ae6f7c605312f5874dbf7b5dd3f5d4aef
Enzo <enzo.haegel@epitech.eu>
2022-06-02 15:31:15 +0200

    update patch item.json

--- LeagueOfStats/src/app/services/riot-pictures.service.ts
+++ LeagueOfStats/src/app/services/riot-pictures.service.ts
@@ -8,50 +8,50 @@ export class RiotPicturesService {
   constructor() { }
 
   public getSummonerIcon(iconId: number): string {
-    return `https://ddragon.leagueoflegends.com/cdn/12.6.1/img/profileicon/${iconId}.png`;
+    return `https://ddragon.leagueoflegends.com/cdn/12.10.1/img/profileicon/${iconId}.png`;
   }
 
   public getChampionIcon(championName: string): string {
-    return `https://ddragon.leagueoflegends.com/cdn/12.6.1/img/champion/${championName}.png`;
+    return `https://ddragon.leagueoflegends.com/cdn/12.10.1/img/champion/${championName}.png`;
   }
 
   public getItemIcon(itemId: number): string {
-    return `https://ddragon.leagueoflegends.com/cdn/12.6.1/img/item/${itemId}.png`;
+    return `https://ddragon.leagueoflegends.com/cdn/12.10.1/img/item/${itemId}.png`;
   }
 
   public getSpellIcon(spellId: string): string {
-    return `https://ddragon.leagueoflegends.com/cdn/12.6.1/img/spell/${spellId}.png`;
+    return `https://ddragon.leagueoflegends.com/cdn/12.10.1/img/spell/${spellId}.png`;
   }
 
   public getRuneIcon(runeId: string): string {
-    return `https://ddragon.leagueoflegends.com/cdn/12.6.1/img/rune/${runeId}.png`;
+    return `https://ddragon.leagueoflegends.com/cdn/12.10.1/img/rune/${runeId}.png`;
   }
 
   public getPerkIcon(perkId: string): string {
-    return `https://ddragon.leagueoflegends.com/cdn/12.6.1/img/perk-icons/${perkId}.png`;
+    return `https://ddragon.leagueoflegends.com/cdn/12.10.1/img/perk-icons/${perkId}.png`;
   }
 
   public getPerkStyleIcon(perkStyleId: string): string {
-    return `https://ddragon.leagueoflegends.com/cdn/12.6.1/img/perk-style/${perkStyleId}.png`;
+    return `https://ddragon.leagueoflegends.com/cdn/12.10.1/img/perk-style/${perkStyleId}.png`;
   }
 
   public getPerkSubStyleIcon(perkSubStyleId: string): string {
-    return `https://ddragon.leagueoflegends.com/cdn/12.6.1/img/perk-substyle/${perkSubStyleId}.png`;
+    return `https://ddragon.leagueoflegends.com/cdn/12.10.1/img/perk-substyle/${perkSubStyleId}.png`;
   }
 
   public getMapIcon(mapId: string): string {
-    return `https://ddragon.leagueoflegends.com/cdn/12.6.1/img/map/${mapId}.png`;
+    return `https://ddragon.leagueoflegends.com/cdn/12.10.1/img/map/${mapId}.png`;
   }
 
   public getEmblemIcon(emblemId: string): string {
-    return `https://ddragon.leagueoflegends.com/cdn/12.6.1/img/profileicon/${emblemId}.png`;
+    return `https://ddragon.leagueoflegends.com/cdn/12.10.1/img/profileicon/${emblemId}.png`;
   }
 
   public getEmblemIconByName(name: string): string {
-    return `https://ddragon.leagueoflegends.com/cdn/12.6.1/img/profileicon/${name}.png`;
+    return `https://ddragon.leagueoflegends.com/cdn/12.10.1/img/profileicon/${name}.png`;
   }
 
   public getEmblemIconBySummonerId(summonerId: string): string {
-    return `https://ddragon.leagueoflegends.com/cdn/12.6.1/img/profileicon/${summonerId}.png`;
+    return `https://ddragon.leagueoflegends.com/cdn/12.10.1/img/profileicon/${summonerId}.png`;
   }
 }
