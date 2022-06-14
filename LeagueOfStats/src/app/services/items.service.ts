f0fd20a51d2738383d7fe3f50af7a621b4adc49b
Enzo Haegel <enzo.haegel@epitech.eu>
2022-06-14 11:58:08 +0200

    items tags

--- LeagueOfStats/src/app/services/items.service.ts
+++ LeagueOfStats/src/app/services/items.service.ts
@@ -3,17 +3,16 @@ import items from '../../assets/item.json';
 import { IItem, IItems } from '../models/IItems';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ItemsService {
-
   public items: IItems = items.data;
 
-  constructor() { }
+  constructor() {}
 
   public getItem(id: string | number): IItem {
     return this.items[id.toString()];
-  } 
+  }
 
   public getDescription(id: string | number): string {
     return this.items[id.toString()].description;
@@ -25,5 +24,40 @@ export class ItemsService {
       items.push(this.items[key]);
     }
     return items;
+  }
+
+  public returnTags(): string[] {
+    return [
+      'LifeSteal',
+      'SpellVamp',
+      'AttackSpeed',
+      'Damage',
+      'Health',
+      'HealthRegen',
+      'ManaRegen',
+      'SpellDamage',
+      'Armor',
+      'Consumable',
+      'Tenacity',
+      'NonbootsMovement',
+      'AbilityHaste',
+      'Mana',
+      'Boots',
+      'CriticalStrike',
+      'ArmorPenetration',
+      'SpellBlock',
+    ];
+  }
+
+  public returnAllTagsArray(): string[] {
+    const tags: string[] = [];
+    for (let key in this.items) {
+      for (let tag of this.items[key].tags) {
+        if (!tags.includes(tag)) {
+          tags.push(tag);
+        }
+      }
+    }
+    return tags;
   }
 }
